3405ff2895d3d6e99888afd99b2e303c3b12f492
Villif <francesco.villi@gmail.com>
2020-12-04 16:20:38 +0100

    change structure of selected colors

--- src/app/selection-line-color/selection-line-color.page.ts
+++ src/app/selection-line-color/selection-line-color.page.ts
@@ -6,43 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./selection-line-color.page.scss'],
 })
 export class SelectionLineColorPage implements OnInit {
-  colors=[
-    {id:0, val: 'verde', coding: "#00ff00" },
-    {id:1, val: 'giallo', coding: "#ffff00" },
-    {id:2, val: 'rosso', coding: "#ff0000" },
-    {id:3, val: 'blu', coding: "#0000ff" },
-    {id:4, val: 'viola', coding: "#800080" },
-    {id:5, val: 'marrone', coding: "#a52a2a" },
-    {id:6, val: 'nero', coding: "#000000" },
-    {id:7, val: 'magenta', coding: "#ff00ff" },
-    {id:8, val: 'rosa', coding: "#ff69b4" },
-    {id:9, val: 'azzurro', coding: "#00ffff" },
-    {id:10, val: 'nullo',  coding: "undefinded" }
+  colors = [
+    { id: 0, val: 'verde', coding: "#00ff00" },
+    { id: 1, val: 'giallo', coding: "#ffff00" },
+    { id: 2, val: 'rosso', coding: "#ff0000" },
+    { id: 3, val: 'blu', coding: "#0000ff" },
+    { id: 4, val: 'viola', coding: "#800080" },
+    { id: 5, val: 'marrone', coding: "#a52a2a" },
+    { id: 6, val: 'nero', coding: "#000000" },
+    { id: 7, val: 'magenta', coding: "#ff00ff" },
+    { id: 8, val: 'rosa', coding: "#ff69b4" },
+    { id: 9, val: 'azzurro', coding: "#00ffff" },
+    { id: 10, val: 'nullo', coding: "undefinded" }
   ];
-  color_A={id:0, val: 'verde', coding: "#00ff00" };
-  color_B={id:1, val: 'giallo', coding: "#ffff00" };
-  color_C={id:2, val: 'rosso', coding: "#ff0000" };
+  colors_selected = [
+    {corsia:'A', id: 0, val: 'verde', coding: "#00ff00" },
+    {corsia:'B' ,id: 1, val: 'giallo', coding: "#ffff00" },
+    {corsia:'C' , id: 2, val: 'rosso', coding: "#ff0000" },
+  ]
+
   constructor() { }
 
   ngOnInit() {
-    if(window.localStorage.getItem("color_A")!=null)
-      this.color_A=JSON.parse(window.localStorage.getItem('color_A'));
-    if(window.localStorage.getItem("color_B")!=null)
-      this.color_B=JSON.parse(window.localStorage.getItem('color_B'));
-    if(window.localStorage.getItem("color_C")!=null)
-      this.color_C=JSON.parse(window.localStorage.getItem('color_C'));
-    
+    if (window.localStorage.getItem("colors_selected") != null)
+      this.colors_selected = JSON.parse(window.localStorage.getItem('colors_selected'));
   }
-  update_color_A(){
-    console.log(this.color_A);
-    window.localStorage.setItem('color_A',JSON.stringify(this.color_A));
+  update_colors_selected() {
+    console.log(this.colors_selected);
+    window.localStorage.setItem('colors_selected', JSON.stringify(this.colors_selected));
   }
-  update_color_B(){
-    console.log(this.color_B);
-    window.localStorage.setItem('color_B',JSON.stringify(this.color_B));
-  }
-  update_color_C(){
-    console.log(this.color_C);
-    window.localStorage.setItem('color_C',JSON.stringify(this.color_C));
-  }
+
 }
