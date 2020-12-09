10da39f488b22771d170adc63b1ba3f460eae348
Villif <francesco.villi@gmail.com>
2020-12-09 18:11:26 +0100

    fix group_radio_button

--- src/app/custom-alert/custom-alert.page.ts
+++ src/app/custom-alert/custom-alert.page.ts
@@ -1,47 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonRadioGroup } from '@ionic/angular'
 @Component({
   selector: 'app-custom-alert',
   templateUrl: './custom-alert.page.html',
   styleUrls: ['./custom-alert.page.scss'],
 })
-export class CustomAlertPage implements OnInit {
-  list_alert=[{
-    id:0,
-    name:'Divieto_accesso',
-    css_class:'rounded-red-class',
-    div_class:'msg',
-    ion_icon_class:'alert',
-    ion_icon_name:'alert'
+export class CustomAlertPage {
+  @ViewChild('radio_gruppo') radio_group: IonRadioGroup;
+  list_alert = [{
+    id: 0,
+    name: 'Divieto_accesso',
+    css_class: 'rounded-red-class',
+    div_class: 'msg',
+    ion_icon_class: 'alert',
+    ion_icon_name: 'alert'
 
-  },{
-    id:1,
-    name:'Default',
-    css_class:'',
-    div_class:'',
-    ion_icon_class:'',
-    ion_icon_name:''
-  },{
-    id:2,
-    name:'test3',
-    css_class:'',
-    div_class:'',
-    ion_icon_class:'',
-    ion_icon_name:''
+  }, {
+    id: 1,
+    name: 'Default',
+    css_class: '',
+    div_class: '',
+    ion_icon_class: '',
+    ion_icon_name: ''
+  }, {
+    id: 2,
+    name: 'test3',
+    css_class: '',
+    div_class: '',
+    ion_icon_class: '',
+    ion_icon_name: ''
   }];
-  selected_radio=this.list_alert[0];
+  selected_radio = this.list_alert[0];
   constructor() {
   }
-  ionViewDidEnter(){
-    this.selected_radio=JSON.parse(window.localStorage.getItem('selected_radio'));
-    if(this.selected_radio==null)
-      this.selected_radio=this.list_alert[0];
+  ionViewDidEnter() {
+    var app = JSON.parse(window.localStorage.getItem('selected_radio'));
+    if (app != null)
+      this.selected_radio = app;
+    console.log(this.selected_radio);
+    this.radio_group.value = this.selected_radio;
   }
   ngOnInit() {
+
   }
-  radioGroupChange(event){
-    this.selected_radio=this.list_alert[event.detail.value.id];
-    console.log(this.selected_radio);
+  radioGroupChange(event) {
+    this.selected_radio = this.list_alert[event.detail.value.id];
     window.localStorage.setItem('selected_radio', JSON.stringify(this.selected_radio));
+    this.radio_group.value = this.selected_radio;
+    //console.log();
   }
 }
