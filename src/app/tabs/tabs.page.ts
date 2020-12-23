0f302f980b2febceff72b3ebfb1f4dc8a0f4ac3d
Villif <francesco.villi@gmail.com>
2020-12-23 13:53:37 +0100

    add notifications badge

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.page.scss'],
 })
 export class TabsPage implements OnInit {
-
-  constructor() { }
+  unread=0;
+  constructor() { 
+  }
 
   ngOnInit() {
+    if(window.localStorage.getItem('unread')!=null)
+      this.unread = JSON.parse(window.localStorage.getItem('unread'));
+  }
+  clear_badge(){
+    this.unread=0;
+    window.localStorage.setItem('unread', JSON.stringify(this.unread));
+  }
+  update_badge(){
+    this.unread++;
+    window.localStorage.setItem('unread', JSON.stringify(this.unread));
   }
 
 }
