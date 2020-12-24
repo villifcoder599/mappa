135504504102bba7273da1c0c619a7cfc0bb1b27
Villif <francesco.villi@gmail.com>
2020-12-24 11:50:13 +0100

    first commit

---
+++ src/app/istruzioni-autorizzazioni/istruzioni-autorizzazioni.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-istruzioni-autorizzazioni',
+  templateUrl: './istruzioni-autorizzazioni.page.html',
+  styleUrls: ['./istruzioni-autorizzazioni.page.scss'],
+})
+export class IstruzioniAutorizzazioniPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
