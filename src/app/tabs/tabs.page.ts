1ad06704589247e7908d4eba7e004e75316c0593
villifCoder559 <francesco.villi@gmail.com>
2021-01-24 17:54:15 +0100

    format doucment

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -6,19 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.page.scss'],
 })
 export class TabsPage implements OnInit {
-  unread=0;
-  constructor() { 
+  unread = 0;
+  constructor() {
   }
 
   ngOnInit() {
-    if(window.localStorage.getItem('unread')!=null)
+    if (window.localStorage.getItem('unread') != null)
       this.unread = JSON.parse(window.localStorage.getItem('unread'));
   }
-  clear_badge(){
-    this.unread=0;
+  clear_badge() {
+    this.unread = 0;
     window.localStorage.setItem('unread', JSON.stringify(this.unread));
   }
-  update_badge(){
+  update_badge() {
     this.unread++;
     window.localStorage.setItem('unread', JSON.stringify(this.unread));
   }
