94302ce8dd6826c645bfbe1eb64ef848a9b4b52e
villifCoder559 <francesco.villi@gmail.com>
2021-01-24 18:02:32 +0100

    add new methods

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -19,29 +19,110 @@ export class DataService {
   ];
   private display_info_autorizz = ['block', 'block', 'block', 'block', 'block']; //0->A 1->B 2->C1 3->C6 4->C7
   private checkboxclose_street = { val: "Corsia riservata nelle vicinanze", isChecked: false };
-
+  private selected_form_alert;
+  private lista_filtro = JSON.parse(JSON.stringify(this.list_authorization));
   constructor() {
     var app = JSON.parse(window.localStorage.getItem('checkboxclose_street'));
     if (app != null)
       this.checkboxclose_street = app;
-    console.log(this.checkboxclose_street)
+    //console.log(this.checkboxclose_street)
+    //this.lista_filtro=JSON.parse(JSON.stringify(this.list_authorization));
   }
-  getListAuthorizzation(){
+  setLastDatasFiltro(data = [{ id: '', val: '', isChecked: false }]) {
+    this.lista_filtro = data;
+  }
+  getLastDatasFiltro() {
+    return this.lista_filtro;
+  }
+  getSelectedFormAlert() {
+    return this.selected_form_alert;
+  }
+  setSelectedFormAlert(data = { id: 0, name: '', css_class: '', div_class: '', ion_icon_class: '', ion_icon_name: '' }) {
+    this.selected_form_alert = data;
+  }
+  getListAuthorizzation() {
     return this.list_authorization;
   }
-  setListAuthorizzation(list){
-    this.list_authorization=list;
+  setListAuthorizzation(list = [{ id: '', val: '', isChecked: false }]) {
+    this.list_authorization = list;
+    this.setLastDatasFiltro(list);
   }
-  setDisplayInfoAutorizz(data) {
+  getNotListAuthorizzation() {
+    var app = JSON.parse(JSON.stringify(this.getLastDatasFiltro()));
+    app.forEach(el => {
+      el.isChecked = !el.isChecked;
+    });
+    return app;
+  }
+  setDisplayInfoAutorizz(data = []) {
     this.display_info_autorizz = data;
+    //console.log(data)
   }
   getDisplayInfoAutorizz() {
     return this.display_info_autorizz;
   }
-  setCheckboxclose_street(value){
-    this.checkboxclose_street=value;
+  setCheckboxclose_street(value = { val: '', isChecked: false }) {
+    this.checkboxclose_street = value;
   }
-  getCheckboxclose_street(){
+  getCheckboxclose_street() {
     return this.checkboxclose_street;
+  }
+  getCorsieFromAutorizzazioni(lista_autorizzazioni = [{ id: '', val: '', isChecked: false }]) {
+    var selected_data = ['none', 'none', 'none', 'none', 'none'];
+    for (var i = 0; i < lista_autorizzazioni.length; i++) {
+      if (lista_autorizzazioni[i].isChecked) {
+        switch (lista_autorizzazioni[i].id) {
+          case 'bus_urb': {
+            selected_data[0] = 'block';
+            selected_data[1] = 'block';
+            selected_data[2] = 'block';
+            break;
+          }
+          case 'bus_extra':
+          case 'hand':
+          case 'ncc':
+          case 'ff_armate':
+          case 'deroga':
+          case 'taxi': {
+            selected_data[0] = 'block';
+            selected_data[1] = 'block';
+            break;
+          }
+          case 'mezzi_op':
+          case 'pol_socc': {
+            selected_data[0] = 'block';
+            selected_data[1] = 'block';
+            selected_data[4] = 'block';
+            break;
+          }
+          case 'autorizz': {
+            selected_data[1] = 'block';
+            selected_data[4] = 'block';
+            break
+          }
+          case 'soccorso': {
+            selected_data[0] = 'block';
+            selected_data[1] = 'block';
+            selected_data[3] = 'block';
+            selected_data[4] = 'block';
+            break
+          }
+        }
+      }
+    }
+    //console.log(selected_data)
+    return selected_data;
+  }
+  createLengendHTMLFromColorsSelected(colors = [{ corsia: '', color: { val: '', coding: '' }, isChecked: true }]) {
+    var innerHTML = '';
+    for (var i = 0; i < colors.length; i++) {
+      if (colors[i].isChecked) {
+        // console.log(colors[i])
+        innerHTML +=
+          '<div class="row"> <i class ="color" style="background:' + colors[i].color.coding + '"></i> ' + '<p id="testo">' +
+          'Corsia ' + colors[i].corsia + ' </p></div>';
+      }
+    }
+    return innerHTML;
   }
 }
