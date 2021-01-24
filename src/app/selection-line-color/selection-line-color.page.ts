bdef1feb2adbd0863f3cb0ee118421a0791f7e34
villifCoder559 <francesco.villi@gmail.com>
2021-01-24 18:03:09 +0100

    fix bugs and add method consigliatoButton

--- src/app/selection-line-color/selection-line-color.page.ts
+++ src/app/selection-line-color/selection-line-color.page.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-selection-line-color',
   templateUrl: './selection-line-color.page.html',
   styleUrls: ['./selection-line-color.page.scss'],
 })
-export class SelectionLineColorPage implements OnInit {
+export class SelectionLineColorPage {
   colors = [
     { val: 'verde', coding: "#00ff00" },
     { val: 'giallo', coding: "#ffff00" },
@@ -17,18 +18,18 @@ export class SelectionLineColorPage implements OnInit {
     { val: 'magenta', coding: "#ff00ff" },
     { val: 'rosa', coding: "#ff69b4" },
     { val: 'azzurro', coding: "#00ffff" },
-    { val: 'nullo', coding: "undefinded" }
+    //{ val: 'nullo', coding: "undefinded" }
   ];
   colors_selected = [
-    { corsia: 'A', color: this.colors[0] },
-    { corsia: 'B', color: this.colors[1] },
-    { corsia: 'C1', color: this.colors[2] },
-    //{ corsia: 'C2', color: this.colors[10] },
-    { corsia: 'C6', color: this.colors[4] },
-    { corsia: 'C7', color: this.colors[3] }
+    { corsia: 'A', color: this.colors[0], isChecked: true },
+    { corsia: 'B', color: this.colors[1], isChecked: true },
+    { corsia: 'C1', color: this.colors[2], isChecked: true },
+    { corsia: 'C6', color: this.colors[4], isChecked: true },
+    { corsia: 'C7', color: this.colors[3], isChecked: true }
   ];
-  constructor() {
-  }
+
+  constructor(private dataService: DataService) { }
+
   get_colors() {
     this.load_data();
     return this.colors_selected;
@@ -36,25 +37,35 @@ export class SelectionLineColorPage implements OnInit {
   load_data() {
     if (window.localStorage.getItem("colors_selected") != null)
       this.colors_selected = JSON.parse(window.localStorage.getItem('colors_selected'));
+    else
+      this.setColorsCorsieFromAutorizzazioni();
   }
-  ngOnInit() {
+  ionViewWillEnter() {
     this.load_data();
     this.show_preview_legend();
   }
   update_colors_selected() {
+    console.log(this.colors_selected)
     window.localStorage.setItem('colors_selected', JSON.stringify(this.colors_selected));
     this.show_preview_legend();
   }
-
-
   show_preview_legend() {
+    console.log(this.colors_selected)
     var element = document.getElementById('preview');
-    element.innerHTML = "";
+    element.innerHTML = this.dataService.createLengendHTMLFromColorsSelected(this.colors_selected);
+  }
+  consigliatiButton() {
+    this.setColorsCorsieFromAutorizzazioni();
+    this.update_colors_selected();
+  }
+  setColorsCorsieFromAutorizzazioni() {
+    var corsie = this.dataService.getCorsieFromAutorizzazioni(this.dataService.getListAuthorizzation());
+    console.log(corsie);
     for (var i = 0; i < this.colors_selected.length; i++) {
-      if (this.colors_selected[i].color.val != 'nullo')
-        element.innerHTML +=
-          '<div class="row"> <i class ="color" style="background:' + this.colors_selected[i].color.coding + '"></i> ' + '<p id="testo">' +
-          'Corsia ' + this.colors_selected[i].corsia + ' </p></div>';
+      if (corsie[i] == 'block')
+        this.colors_selected[i].isChecked = false;
+      else
+        this.colors_selected[i].isChecked = true;
     }
   }
 }
