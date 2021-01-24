9e64fdcfb9d5e63d1898c02c5c82b160fe2e6177
villifCoder559 <francesco.villi@gmail.com>
2021-01-24 17:58:35 +0100

    fix bug when i read var from shared page

--- src/app/istruzioni-autorizzazioni/istruzioni-autorizzazioni.page.ts
+++ src/app/istruzioni-autorizzazioni/istruzioni-autorizzazioni.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
-
+import { FiltroIstruzioniAutorizzazionePage } from '..//filtro-istruzioni-autorizzazione/filtro-istruzioni-autorizzazione.page'
 @Component({
   selector: 'app-istruzioni-autorizzazioni',
   templateUrl: './istruzioni-autorizzazioni.page.html',
@@ -9,15 +9,33 @@ import { DataService } from '../services/data.service';
 })
 export class IstruzioniAutorizzazioniPage implements OnInit {
 
-  constructor(private router: Router, private dataService: DataService) { }
+  constructor(private router: Router, private dataService: DataService, private filtro: FiltroIstruzioniAutorizzazionePage) { }
 
   ngOnInit() {
 
   }
+  setDefaultListFiltro() {
+    var def = ['none', 'none', 'none', 'none', 'none'];
+    this.dataService.setDisplayInfoAutorizz(def);
+  }
   //0->A 1->B 2->C1 3->C6 4->C7
   ionViewDidEnter() {
+    //var view = this.dataService.getCorsieFromAutorizzazioni(this.dataService.getLastDatasFiltro());
     var view = this.dataService.getDisplayInfoAutorizz();
     console.log(view)
+    var change = false;
+    for (var i = 0; i < view.length; i++) {
+      if (view[i] == 'block') {
+        change = true;
+        //  view[i] = 'none'
+      }
+      // else
+      //   view[i] = 'block';
+    }
+    if (!change) {
+      view = ['block', 'block', 'block', 'block', 'block'];
+    }
+    //console.log(view)
     for (var i = 0; i < view.length; i++) {
       switch (i) {
         case 0: { document.getElementById('A').style.display = view[i]; break; }
